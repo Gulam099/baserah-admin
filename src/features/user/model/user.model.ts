b629f42d811b4f61758f5a57e69c1aa17bb9f129
Techmier <72434947+Zahid40@users.noreply.github.com>
2025-03-17 16:56:51 +0530

    Update user.model.ts

--- src/features/user/model/user.model.ts
+++ src/features/user/model/user.model.ts
@@ -26,6 +26,6 @@ const UserSchema = new Schema<User>(
 );
 
 // Create and export the User model
-const User = mongoose.models.User || mongoose.model<User>("User", UserSchema);
+const User = mongoose.models.User || mongoose.model<User>("User", UserSchema , "admins");
 
 export default User;
