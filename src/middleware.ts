685b739d204fb12b4aa57d46e25d41e198281348
Techmier <72434947+Zahid40@users.noreply.github.com>
2025-03-17 18:03:55 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,11 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/api(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   const { userId, redirectToSignIn } = await auth();
-  if (!userId && !isPublicRoute(request)) {
+  // Protect the root `/` route explicitly
+  if (
+    !userId &&
+    (request.nextUrl.pathname === "/" || !isPublicRoute(request))
+  ) {
     await auth.protect();
     return redirectToSignIn();
   }
@@ -12,6 +16,7 @@ export default clerkMiddleware(async (auth, request) => {
 
 export const config = {
   matcher: [
+    "/",
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
