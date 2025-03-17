9693683af6423fee824ba6ae53ad9399184d04cf
Techmier <72434947+Zahid40@users.noreply.github.com>
2025-03-17 16:46:27 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-])
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/api(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  const { userId, redirectToSignIn } = await auth()
+  const { userId, redirectToSignIn } = await auth();
   if (!userId && !isPublicRoute(request)) {
-    await auth.protect()
-    return redirectToSignIn()
+    await auth.protect();
+    return redirectToSignIn();
   }
-})
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
