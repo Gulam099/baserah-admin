19f72ab4df8daab906bd120e5e869457b760ea14
talhaansari692 <talhaansari692@gmail.com>
2025-07-02 19:48:34 +0530

    this is the code of latest

--- const.ts
+++ const.ts
@@ -1,4 +1,4 @@
 export const appName = "Baserah";
-export const ApiBaseUrl = "https://16.24.148.188";
-export const ApiBaseUrlLocal = "https://16.24.148.188";
+export const ApiBaseUrl = "https://server.baserah.sa";
+export const ApiBaseUrlLocal = "https://server.baserah.sa";
 
