ac4fc73508be6d4cad73cc40cc21c7dd83a01a24
mohsinhamza7248 <mohsinhamza7248@gmail.com>
2025-08-04 18:15:51 +0530

    update api link

--- const.ts
+++ const.ts
@@ -1,5 +1,4 @@
 export const appName = "Baserah";
 
 export const ApiBaseUrl = "https://server.baserah.sa";
-// export const ApiBaseUrlLocal = "https://server.baserah.sa";
-export const ApiBaseUrlLocal = "http://localhost:4000";
+export const ApiBaseUrlLocal = "https://server.baserah.sa";
