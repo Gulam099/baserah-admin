ea567838afc8eb6e668d99b70a1576d02e0e19bb
talhaansari692 <talhaansari692@gmail.com>
2026-03-11 15:48:18 +0530

    Update const.ts with live url

--- const.ts
+++ const.ts
@@ -1,4 +1,4 @@
 export const appName = "Baserah";
 
-export const ApiBaseUrl = "http://192.168.1.13:4000";
-export const ApiBaseUrlLocal = "http://192.168.1.13:4000";
+export const ApiBaseUrl = "https://server.baserh.sa";
+export const ApiBaseUrlLocal = "https://server.baserh.sa";
